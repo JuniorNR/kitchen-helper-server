3fa48f1d7fd7da94717286170450d94c11caddde
Oleg Kudelkin <tryhard.work@mail.ru>
2025-10-21 14:39:16 +0300

    feat: fix

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c "php artisan optimize && php artisan migrate --force && (php artisan storage:link || true) && heroku-php-apache2 public/"
--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-        env('FRONTEND_URL_2', 'http://127.0.0.1:3000'),
+        env('FRONTEND_URL_2', 'https://kitchen-helper52.netlify.app'),
+        env('FRONTEND_URL', 'http://127.0.0.1:3000'),
     ],
 
     'allowed_origins_patterns' => [],
